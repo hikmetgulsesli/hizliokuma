d62fd99fad7f8e5d318976fabb901e23ce1d4837
Moltclaw AI <setrox@moltclaw.local>
2026-02-19 03:59:53 +0300

    Add missing dependencies: tailwindcss-animate, ts-jest; fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    }],
+  },
 }
